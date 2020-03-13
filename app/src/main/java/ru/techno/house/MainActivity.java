3d4583ca6fa3575ff5f8136e18c210d379dc9766
com-techno <59176394+com-techno@users.noreply.github.com>
2020-03-13 22:08:25 +0300

    techno

--- app/src/main/java/ru/techno/house/MainActivity.java
+++ app/src/main/java/ru/techno/house/MainActivity.java
@@ -1,11 +1,24 @@
 package ru.techno.house;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private boolean isMainOpened = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,20 +27,66 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         findViewById(R.id.control).setOnClickListener(this);
         findViewById(R.id.settings).setOnClickListener(this);
         findViewById(R.id.about).setOnClickListener(this);
+//        findViewById(R.id.back).setOnClickListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (isMainOpened){
+            openQuitDialog();
+        } else {
+            isMainOpened = true;
+            setContentView(R.layout.activity_main);
+            findViewById(R.id.control).setOnClickListener(this);
+            findViewById(R.id.settings).setOnClickListener(this);
+            findViewById(R.id.about).setOnClickListener(this);
+        }
+    }
+
+    @SuppressLint("InflateParams")
+    private void openQuitDialog() {
+        final AlertDialog.Builder quitDialog = new AlertDialog.Builder(MainActivity.this);
+        quitDialog.setView(R.layout.quit_dialog);
+        quitDialog.show();
+        final AlertDialog alert = quitDialog.create();
+        /*alert.show();
+        alert.setButton(R.id.no_quit, "Lol", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });*/
+        if (findViewById(R.id.no_quit) != null && findViewById(R.id.quit) != null) {
+            findViewById(R.id.no_quit).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    alert.dismiss();
+                }
+            });
+            findViewById(R.id.quit).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    finish();
+                }
+            });
+        }
     }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.control:{
+        switch (v.getId()) {
+            case R.id.control: {
+                isMainOpened = false;
                 setContentView(R.layout.control);
                 break;
             }
-            case R.id.settings:{
+            case R.id.settings: {
+                isMainOpened = false;
                 setContentView(R.layout.settings);
                 break;
             }
-            case R.id.about:{
+            case R.id.about: {
+                isMainOpened = false;
                 setContentView(R.layout.about);
                 break;
             }
--- app/src/main/java/ru/techno/house/SplashActivity.java
+++ app/src/main/java/ru/techno/house/SplashActivity.java
@@ -3,6 +3,9 @@ package ru.techno.house;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
@@ -10,11 +13,29 @@ public class SplashActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.hello);
+        setContentView(R.layout.hello);/*
+        Toast.makeText(this, "lol", Toast.LENGTH_SHORT).show();
+        for (int i = 0; i< 10000; i++) Log.i("lol", i + "");
+        Toast.makeText(this, "lol10000", Toast.LENGTH_SHORT).show();*/
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //TODO put here code to exec before launching MainActivity
 
 
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-        finish();
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                SplashActivity.this.startActivity(intent);
+                SplashActivity.this.finish();
+            }
+        }, 1000);
+        /*new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                SplashActivity.this.startActivity(intent);
+                SplashActivity.this.finish();
+            }
+        }, 1000);*/
     }
 }
